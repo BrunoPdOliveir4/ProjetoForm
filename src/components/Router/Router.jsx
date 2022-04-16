38760c3bae31cc634c1b4d857faa674130b82460
jcjaber <jcjaber@gmail.com>
2022-04-16 10:20:07 -0300

    added contributors

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -3,10 +3,7 @@ import {
   ErrorPage,
   HomePage,
   LobbyPage,
-  LoginPage,
-  ProfilePage,
   RegisterPage,
-  UsersPage,
 } from '../../pages';
 import DogPage from '../../pages/Dog.page';
 
@@ -14,12 +11,8 @@ const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/lobby" element={<LobbyPage />} />
-      <Route path="/users" element={<UsersPage />} />
-      <Route path="/dog" element={<DogPage />} />
-      <Route path="/profile/:userId" element={<ProfilePage />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   </BrowserRouter>
