8f5ac08d6bee5adb9b8a75ac43128a0fb25eb034
Ana Carolina Luz <anacarolinaluz@id.uff.br>
2022-04-26 12:19:40 -0300

    css registerpage

--- src/components/DefaultInput/DefaultInput.style.js
+++ src/components/DefaultInput/DefaultInput.style.js
@@ -2,21 +2,36 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
 ${() => css`
-display: flex;
-justify-content: center;
-align-items: center;`
+box-sizing:content-box;
+display: grid;
+place-items: center;
+margin: 0 auto;
+padding: 10px 0 10px 0;
+border: 1px;
+min-width: 100%;
+height: 10vh;
+border: 5vw;
+/* border: 1px solid red; */
+`
 }
 `;
 export const Label = styled.label`
 ${() => css`
-padding: 5px;
-margin: 0auto;
+display:block;
+padding: .1px .5px .1px .5px;
+font-size: 20px;
+margin: 0 0 .3vh 0;
 `
 }
 `;
 export const Input = styled.input`
 ${() => css`
-margin: 0auto;
+display: block;
+width: 50%;
+padding: .3rem .8rem .3rem .8rem;
+margin: .9vw 0 ;
+border-radius: 5px;
+font-size: 20px;
 `
 }
 `;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -28,6 +28,28 @@ const GlobalStyle = createGlobalStyle`
     color: black;
 
   }
+
+  input[type="button"]{
+    display: flex;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    margin: 40px auto;
+    width: 30%;
+    height: 2rem;
+    color: white;
+    border: 0;
+    border-radius: 5px;
+    bottom: 0;
+    cursor: pointer;
+    font-size: 20px;
+    background-color: #1D5297;
+    transition: background-color 500ms ease-in-out;
+  }
+
+  input:hover[type="button"]{
+    background-color: #1D1297;
+  }
 `;
 
 export default GlobalStyle;
